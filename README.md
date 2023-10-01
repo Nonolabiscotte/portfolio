4b9c8c291aa7f9b7f37b18f3d9e75ba2eb46d3b3
Nonolabiscotte <nonomaechler@gmail.com>
2023-10-01 12:38:20 +0200

    contact about portfolio component generation

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # portfolio
+
+Angular Portfolio Project by Noé Maechler
+
+Launch project :
+git clone
+open with your ide
+cd portfolio
+ng serve
+go to : http://localhost:4200/
--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -1,15 +1,35 @@
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ExperimentComponent } from './components/experiment/experiment.component';
 import { EducationComponent } from './components/education/education.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ResumeComponent } from './components/resume/resume.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+
+const routes: Routes = [
+  {path: 'app-about', component: AboutComponent},
+  {path: 'app-portfolio', component: PortfolioComponent},
+  {path: 'app-resume', component: ResumeComponent},
+  {path: 'app-contact', component: ContactComponent}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     ExperimentComponent,
-    EducationComponent
+    EducationComponent,
+    HeaderComponent,
+    FooterComponent,
+    ResumeComponent,
+    AboutComponent,
+    ContactComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule
